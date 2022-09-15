7a3e5b0cf11cca5a50d2d094f388588de306d72f
Roman Likhomanov <romanlee87@gmail.com>
2022-09-15 11:51:31 +0300

    corrections

--- src/main/ui/App.tsx
+++ src/main/ui/App.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import './App.css';
-import { HashRouter } from 'react-router-dom';
+import {HashRouter} from 'react-router-dom';
 
 import Main from './Main';
+import {Provider} from 'react-redux';
+import {store} from '../bll/store';
 
 const App = () => {
     return (
         <div>
             <HashRouter>
-
+                <Provider store={store}>
                     <Main/>
-
+                </Provider>
             </HashRouter>
         </div>
     );
