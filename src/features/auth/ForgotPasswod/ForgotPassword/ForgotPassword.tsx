8bf96f5a60b0d77defdfec1bdca5ab9e78d8c390
Roman Likhomanov <romanlee87@gmail.com>
2022-10-30 17:46:34 +0300

    refactor: forgotPassword

--- src/features/auth/ForgotPasswod/ForgotPassword/ForgotPassword.tsx
+++ src/features/auth/ForgotPasswod/ForgotPassword/ForgotPassword.tsx
@@ -24,7 +24,7 @@ export const ForgotPassword = () => {
             email: '',
             message: `<div style="background-color: lime; padding: 15px">
                         password recovery link: 
-                       <a href='http://localhost:3000/#/set-new-password/$token$'>
+                       <a href='https://roman-likhomanov.github.io/cards/#/set-new-password/$token$'>
                         link</a>
                         </div>`
         },
